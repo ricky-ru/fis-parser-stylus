878a8211aea41ce3ac7f1d056e07cfe89c96a193
luyujun <luyujun87@gmail.com>
2013-07-31 22:08:37 +0800

    set css compress default

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var stylus = require('stylus');
 
 module.exports = function(content, file, conf){
   var fn;
-  stylus.render(content, function(err, css){
+  stylus.render(content, {compress: true}, function(err, css){
     if (err) throw err;
     fn = css;
   });
